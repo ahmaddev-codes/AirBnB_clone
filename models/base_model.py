c65d6ba3e68af5d46868bb45542e6075e1f13844
iahm-codes <omegabliss23@.com>
2024-01-10 06:31:51 +0100

    Code: updated __init__.py file_storage and base_model

--- models/base_model.py
+++ models/base_model.py
@@ -7,6 +7,7 @@ all common attributes/methods for other classes
 
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
@@ -29,7 +30,7 @@ class BaseModel:
                     self.created_at = datetime.strptime(kwargs['created_at'],
                                                         time_format)
                 elif key == 'updated_at':
-                    self.created_at = datetime.strptime(kwargs['created_at'],
+                    self.created_at = datetime.strptime(kwargs['updated_at'],
                                                         time_format)
                 else:
                     setattr(self, key, value)
@@ -38,25 +39,32 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            # Call the new method on storage for a new instance
+            models.storage.new(self)
 
     def save(self):
         """Updates the public instance attribute updated_at with
         the current datetime
         """
         self.updated_at = datetime.now()
+        models.storage.save()  # call the save method of storage
 
     def to_dict(self):
         """Returns a dictionary representation of the instance."""
+
         # Copy the class in a new object
         my_dict = self.__dict__.copy()
 
         # Add '__class__' key with the class name
         my_dict['__class__'] = self.__class__.__name__
 
+        # Remove '__class__' from the dictionary
+        my_dict.pop('__class__', None)
+
         # Convert 'created_at' and 'updated_at' to ISO format strings
-        for key, value in my_dict.items():
-            if key in ['created_at', 'updated_at']:
-                my_dict[key] = value.isoformat()
+        my_dict['created_at'] = self.created_at.isoformat()
+        my_dict['updated_at'] = self.updated_at.isoformat() if hasattr(
+            self, 'updated_at') else None
 
         return my_dict
 
---
+++ models/engine/file_storage.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""This module contains a Filestorage class that serializes
+instances to a JSON file and deserializes JSON files to
+instances
+"""
+
+
+import json
+import os
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """The file storage engine class:
+    A class that serialize and deserialize instances to a JSON file
+    """
+    class_dict = {"BaseModel": BaseModel}
+
+    def __init__(self):
+        """Initialization of a FileStorage Instance"""
+
+        self.__file_path = "file.json"
+        self.__objects = {}
+
+    def all(self):
+        """Returns the dictionary of objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """Sets new obj in __objects dictionary."""
+        if obj.id in self.__objects:
+            print("exists")
+            return
+
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file (path: __file_path)"""
+        new_dict = []
+
+        for obj in self.__objects.values():
+            new_dict.append(obj.to_dict())
+
+        with open(self.__file_path, "w", encoding='utf-8') as file:
+            json.dump(new_dict, file)
+
+    def reload(self):
+        """Deserializes the JSON file to __objects if it exists"""
+
+        # Check if the path to file exists
+        if os.path.exists(self.__file_path) is True:
+            return
+
+            try:
+                with open(self.__file_path, "r") as file:
+                    new_obj = json.load(file)
+
+                    for key, val in new_obj.items():
+                        obj = self.class_dict[val['__class__']](**val)
+                        self.__objects[key] = obj
+                        # OR - dictionary comprehension
+                        # self.__objects =
+                        # {key: self.classes[obj['__class__']](**obj) \
+                        # for key, obj in loaded_objects.items()}
+            except Exception:
+                pass
